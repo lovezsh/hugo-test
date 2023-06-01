2b43be8ecdf9a50fd0134560e1cbdbba89b706cd
lovezsh <shuxhan@163.com>
2023-06-01 20:46:35 +0800

    提交于 undefined

--- HugoFast.config.js
+++ HugoFast.config.js
@@ -1,1 +1,1 @@
-{"domain":"https://funny-haupia-b69f10.netlify.app/","name":"管理员","authorimg":"https://img.zburu.com/author.png"}
+{"domain":"https://funny-haupia-b69f10.netlify.app/","name":"子舒","authorimg":"https://img.zburu.com/author.png"}
